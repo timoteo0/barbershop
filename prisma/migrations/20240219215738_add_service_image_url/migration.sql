30a816daad75fee16bdc171a883189bcc94e6e27
Timoteo Alves do Nascimento <timoteonascimento8@gmail.com>
2024-02-20 14:16:53 -0300

    fix: fix service image_url migration

--- prisma/migrations/20240219215738_add_service_image_url/migration.sql
+++ prisma/migrations/20240219215738_add_service_image_url/migration.sql
@@ -1,8 +1,3 @@
-/*
-  Warnings:
 
-  - Added the required column `imageUrl` to the `Service` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
 ALTER TABLE "Service" ADD COLUMN     "imageUrl" TEXT NOT NULL;
