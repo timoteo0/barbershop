8c382272118e05ff97fc8824ae88e9d6a571bccb
Timoteo Alves do Nascimento <timoteonascimento8@gmail.com>
2024-02-21 21:08:18 -0300

    feat: add bookings page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -31,7 +31,7 @@ export default async function Home() {
           Agendamentos
         </h2>
 
-        <BookingItem />
+        {/*<BookingItem />*/}
       </div>
 
       <div className="mt-6">
